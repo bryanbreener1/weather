a2733ba781cd339997b72a118803ea1422229eb0
breenerPC <bquispevaleriano@gmail.com>
2023-06-15 18:19:15 -0500

    api request finished, ready to add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import useFetch from './hooks/useFetch'
+import getApiKey from './utils/getApiKey'
+import axios from 'axios';
 
 
 function App() {
   
   const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temp, setTemp] = useState()
 
   
   useEffect(()=>{
@@ -18,10 +21,39 @@ function App() {
     }
     navigator.geolocation.getCurrentPosition(success)
   },[])
-  
-  console.log(coords);
+
+  useEffect(()=>{
+    if(coords){
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${getApiKey()}`
+
+      axios.get(url)
+        .then(res => {
+          setWeather(res.data)
+          setTemp(res.data.main.temp)
+        })
+        .catch((err)=> console.log(err))
+    }
+  },[coords])
+
+  console.log(weather);
   return (
-    <div>App</div>
+    <article>
+      <h1>Weather App</h1>
+      <h2>{weather?.name}, {weather?.sys.country}</h2>
+      <section>
+        <img src={weather ? `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png` : ''} alt="va un icono" />
+        <div className='wrapper'>
+          <h3>"{weather?.weather[0].description}"</h3>
+          <ul>
+            <li><span>Wind Speed</span><span>{weather?.wind.speed}</span></li>
+            <li><span>Clouds</span><span>{weather?.clouds.all}</span></li>
+            <li><span>Pressure</span><span>{weather?.main.pressure}</span></li>
+          </ul>
+        </div>
+      </section>
+      <aside>{temp}</aside>
+      <button>Change to °C</button>
+    </article>
   )
 }
 
---
+++ src/utils/getApiKey.js
@@ -0,0 +1,2 @@
+const getApiKey = () => '6bbf25dbfa758f23a18417ae2982607f'
+export default getApiKey
